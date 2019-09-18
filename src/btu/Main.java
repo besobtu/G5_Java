4a6a1c79715a14fb4e4602836c357b8c3dcae1ac
student <besik.tabatadze@btu.edu.ge>
2019-09-18 12:45:28 +0400

    Test

---
+++ src/btu/Main.java
@@ -0,0 +1,10 @@
+package btu;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Java");
+        System.out.println("C++");
+        System.out.println("C#");
+    }
+}
